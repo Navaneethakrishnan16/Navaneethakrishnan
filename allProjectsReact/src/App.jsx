618e74d7e84ea377678bcbfdfc0c2fc60abfc7ac
myusername <webdeveloper2@amvionlabs.com>
2024-08-03 11:39:31 +0530

    added react

---
+++ allProjectsReact/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import ReactReveal from './Component/ReactReveal/ReactReveal'
+
+function App() {
+ 
+
+  return (
+    <ReactReveal></ReactReveal>
+  )
+}
+
+export default App
---
+++ allProjectsReact/src/Component/ReactReveal/ReactReveal.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+import '../ReactReveal/ReactReveal.css'
+
+function ReactReveal() {
+  return (
+    <div className='verticaltimeline'>
+        <VerticalTimeline >
+            <VerticalTimelineElement
+            className='vertical-timeline-element'
+            >
+                <h2>Hello</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ad eum asperiores distinctio, minus aspernatur sint rem dolore non, doloribus autem esse consequatur. Ullam impedit earum, laborum pariatur dolorem est!</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+            className='vertical-timeline-element'
+            >
+                <h2>Hello</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ad eum asperiores distinctio, minus aspernatur sint rem dolore non, doloribus autem esse consequatur. Ullam impedit earum, laborum pariatur dolorem est!</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+            className='vertical-timeline-element'
+            >
+                <h2>Hello</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ad eum asperiores distinctio, minus aspernatur sint rem dolore non, doloribus autem esse consequatur. Ullam impedit earum, laborum pariatur dolorem est!</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+            className='vertical-timeline-element'
+            >
+                <h2>Hello</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ad eum asperiores distinctio, minus aspernatur sint rem dolore non, doloribus autem esse consequatur. Ullam impedit earum, laborum pariatur dolorem est!</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+            className='vertical-timeline-element'
+            >
+                <h2>Hello</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ad eum asperiores distinctio, minus aspernatur sint rem dolore non, doloribus autem esse consequatur. Ullam impedit earum, laborum pariatur dolorem est!</p>
+            </VerticalTimelineElement>
+        </VerticalTimeline>
+    </div>
+  )
+}
+
+export default ReactReveal
